697e4a5ba421296ffe2e59c257d0be63e2681201
Titanic880 <Warframeia1501@gmail.com>
2021-04-15 18:25:24 -0600

    Finished Documenting objects, started documenting client

--- Quiz_Client/Host_Connection.cs
+++ Quiz_Client/Host_Connection.cs
@@ -1,15 +1,11 @@
 ﻿using System.Runtime.Serialization.Formatters.Binary;
 using System.Runtime.Serialization;
-using System.Collections.Generic;
-using System.Threading.Tasks;
 using System.ComponentModel;
 using System.Net.Sockets;
-using System.Text;
-using System.Linq;
 using System.IO;
-using System;
-using Standards_Final.Network;
+
 using Standards_Final.Sessions;
+using Standards_Final.Network;
 using Standards_Final.Quizlet;
 using Standards_Final.Users;
 
@@ -20,25 +16,46 @@ namespace Quiz_Client
         private readonly string IPAddress;
 
         #region Delegates
-        //Recieves data from the server
+        /// <summary>
+        /// Recieves Generalized data from the server (Depreciated)
+        /// </summary>
         public event RecievedFromServer FromServer;
         public delegate void RecievedFromServer(object package);
 
+        /// <summary>
+        /// Handles the Login Result
+        /// </summary>
         public event ResultLogin Login_Res;
         public delegate void ResultLogin(Login_Result result);
 
-        public event RecievedQuiz GetQuiz;
-        public delegate void RecievedQuiz(Quiz[] le_Quiz);
-
+        /// <summary>
+        /// Recieves the Active Question
+        /// </summary>
         public event RecievedQuestion QuestionGet;
-        public delegate void RecievedQuestion(Question Q);
+        public delegate void RecievedQuestion(Active_Question Q);
 
+        /// <summary>
+        /// Gets the Session from Server (Depreciated?)
+        /// </summary>
         public event Session GetSession;
         public delegate void Session(Session_Conn session);
 
+
+        /// <summary>
+        /// Recieves a list of Quizzes for displaying
+        /// </summary>
+        public event RecievedQuiz GetQuiz;
+        public delegate void RecievedQuiz(Quiz[] le_Quiz);
+
+        /// <summary>
+        /// Recieves a list of Questions for displaying
+        /// </summary>
         public event GetQuizQuest GetQuestion;
         public delegate void GetQuizQuest(Question[] Q);
 
+        /// <summary>
+        /// Recieves a list of users for Displaying
+        /// </summary>
         public event GetUserConnList GetList;
         public delegate void GetUserConnList(User[] Connected);
         #endregion Delegates
@@ -48,36 +65,63 @@ namespace Quiz_Client
         private NetworkStream nStream;
         private BinaryWriter writer;
 
+        /// <summary>
+        /// Controls the main Loop of Talking with the server
+        /// </summary>
         readonly private BackgroundWorker wkr = new BackgroundWorker();
 
+        /// <summary>
+        /// MAIN CONSTRUCTOR
+        /// </summary>
+        /// <param name="IP"></param>
         public Host_Connection(string IP)
         {
+            //Sets the IP to be used to talk with the server
             IPAddress = IP;
 
+            //Configures the worker
             wkr.WorkerSupportsCancellation = true;
             wkr.DoWork += MainLoop;
 
+            //Runs the worker
             wkr.RunWorkerAsync();
         }
         private void MainLoop(object s, DoWorkEventArgs e)
         {
+            //Configures the client's TCP Information
             tcpClient = new TcpClient();
+            //Attempts to connect to the given IP:Port
             tcpClient.Connect(IPAddress, Standards_Final.Standards.Port);
 
+            //hooks onto the hosts data stream
             nStream = tcpClient.GetStream();
 
-            if (nStream == null)
-                return;
-
+            //Sets up the Stream Writer
             writer = new BinaryWriter(nStream);
+            //Creates the Stream to pull information from
             IFormatter Stream = new BinaryFormatter();
 
+            //MAIN LOOP
             while (!wkr.CancellationPending)
             {
+                //Checks if the stream is broken
                 if (nStream == null)
-                    continue;
+                {
+                    //Reconfigures the stream
+                    nStream = tcpClient.GetStream();
+                    writer = new BinaryWriter(nStream);
+                    //continue; // DEPRECIATED?
+                }
+                //Pulls an item from the stream
                 object o = Stream.Deserialize(nStream);
 
+                //I WISH THIS COULD BE A SWITCH
+                switch (o)
+                {
+                    case string:
+
+                        break;
+                }
                 if (o is Login_Result LR)
                     Login_Res(LR);
                 else if (o is Quiz[] Q)
